00044f98afd8dfcca7b0d1d00af06ce3fba59012
Ubuntu <ubuntu@ip-172-31-40-227.ec2.internal>
2026-04-29 13:43:23 +0000

    My first script

---
+++ Day1/intro.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This is just an example of writing bassic script
+#
+echo "user: hey!, what is it for"
+echo "Abhi: Hello! This is just for practicing the shell scripting"
+echo "User: And why are you into shell"
+echo "Abhi: because I am learning DevOps and cloud computing"
